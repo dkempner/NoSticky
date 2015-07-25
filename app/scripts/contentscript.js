5c705f9f2a25f8dbd1e4d3a57db8563e93e5d589
Daniel Kempner <djkempner@gmail.com>
2015-07-24 19:55:50 -0700

    document ready

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,33 +1,35 @@
-var windowWidth = window.innerWidth;
-var $body = $('body');
-var style = document.createElement('style');
-style.innerHTML = "#noSticky {position: relative !important; width: 100% !important}";
-$body.append(style);
-var stickies = $('*').filter(function() {
-    var $this = $(this);
-    var isFixed = $this.css('position').toLowerCase().indexOf('fixed') > -1;
-    if (!isFixed) return false;
-    var top = $this.css('top').toLowerCase();
-    top = top.replace('px', '');
-    top = top.replace('%', '');
-    top = top.replace(' ', '');
-    var num;
-    try {
-        num = parseInt(top, 0);
-    } catch (e) {
-        return false;
-    }
+$(function () {
+    var windowWidth = window.innerWidth;
+    var $body = $('body');
+    var style = document.createElement('style');
+    style.innerHTML = "#noSticky {position: relative !important; width: 100% !important}";
+    $body.append(style);
+    var stickies = $('*').filter(function () {
+        var $this = $(this);
+        var isFixed = $this.css('position').toLowerCase().indexOf('fixed') > -1;
+        if (!isFixed) return false;
+        var top = $this.css('top').toLowerCase();
+        top = top.replace('px', '');
+        top = top.replace('%', '');
+        top = top.replace(' ', '');
+        var num;
+        try {
+            num = parseInt(top, 0);
+        } catch (e) {
+            return false;
+        }
 
-    var isCloseToTop = num < 5 && num > -5;
-    if (!isCloseToTop) return false;
+        var isCloseToTop = num < 5 && num > -5;
+        if (!isCloseToTop) return false;
 
-    var width = $this.width();
-    var difference = windowWidth - width;
-    var percent = difference / windowWidth;
-    var within10Percent = percent <= 0.1;
+        var width = $this.width();
+        var difference = windowWidth - width;
+        var percent = difference / windowWidth;
+        var within10Percent = percent <= 0.1;
 
-    return within10Percent;
+        return within10Percent;
 
+    });
+
+    stickies.attr('id', 'noSticky');
 });
-
-stickies.attr('id', 'noSticky');
